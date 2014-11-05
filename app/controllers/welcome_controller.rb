6cd6785c30ba9955d83642b72d713d203fd5a1ba
Kim Chen <killworm737@yahoo.com.tw>
2014-11-05 18:08:12 +0800

    update ruby 2.1.4

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,30 @@
 class WelcomeController < ApplicationController
   def index
+      @high_scores = Welcome.all
+  end       
+  def show
+
   end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
 end
